6d0a110e3019201f4281cbb09890636731b11559
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-19 16:40:02 +0530

    [user registration]check Email is invalid or not

---
+++ Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistr19
+{
+    internal class Email
+    {
+        public static void ValidateEmail()
+        {
+            Console.WriteLine("Enter EmailID");
+            string email = Console.ReadLine();
+
+            string pattern = @"^([a-z]+)(\.[a-z0-9_\+\-]+)?@([a-z]+)\.([a-z]{2,5)(\.[a-z]{2})?$";
+
+            if (Regex.IsMatch(email, pattern))
+            {
+                Console.WriteLine("Email Id is valid");
+            }
+            else
+            {
+                Console.WriteLine("Email Id is not valid");
+            }
+        }
+    }
+}
